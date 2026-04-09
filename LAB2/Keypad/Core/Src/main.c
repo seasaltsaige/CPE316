b45253c750058066ed4ebb66de77bba8132b384b
seasaltsaige <oriontothemax@gmail.com>
2026-04-09 15:58:57 -0700

    lab 2 working

--- LAB2/Keypad/Core/Src/main.c
+++ LAB2/Keypad/Core/Src/main.c
@@ -1,21 +1,3 @@
-/* USER CODE BEGIN Header */
-/**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Main program body
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2026 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
-
 #include "main.h"
 #include "stm32l476xx.h"
 #include "stm32l4xx_hal.h"
@@ -23,69 +5,57 @@
 #include <stdint.h>
 
 #define ERR_KEYPAD -1
-const uint16_t COL_PINS[] = { GPIO_PIN_3, GPIO_PIN_4, GPIO_PIN_5 };
+
+#define NUM_COLS 3
+#define NUM_ROWS 4
+
+const uint16_t COL_PINS[NUM_COLS] = { GPIO_PIN_3, GPIO_PIN_4, GPIO_PIN_5 };
 #define KEYPAD_COLS (GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5)
-const uint16_t ROW_PINS[] = { GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_2, GPIO_PIN_3 };
-#define KEYPAD_ROWS (GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3)
+
+const uint16_t ROW_PINS[NUM_ROWS] = { GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_9, GPIO_PIN_10 };
+#define KEYPAD_ROWS (GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_9 | GPIO_PIN_10)
 
 
-const int keypad_map[4][3] = {
+#define LED_PIN_MASK (GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8)
+
+// Note: [3][0] = '*' (display 14)
+// [3][2] = '#' (display 15)
+const int keypad_map[NUM_ROWS][NUM_COLS] = {
   {1, 2, 3},
   {4, 5, 6},
   {7, 8, 9},
-  {14, 0, 15} // *, 0, #
+  {14, 0, 15}
 };
 
-UART_HandleTypeDef huart2;
-
 void SystemClock_Config(void);
-static void MX_USART2_UART_Init(void);
+int16_t read_keypad();
+void init_gpio();
 
-int read_keypad();
-
-/**
- * @brief  The application entry point.
- * @retval int
- */
 int main(void) {
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
-   */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
   /* Configure the system clock */
   SystemClock_Config();
-
-  MX_USART2_UART_Init();
-
   RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOAEN);
 
-  // ROWS  input w/ pull-downs
-  GPIOA->MODER &= ~(GPIO_MODER_MODE0 | GPIO_MODER_MODE1 | GPIO_MODER_MODE2 | GPIO_MODER_MODE3 | GPIO_MODER_MODE5 | GPIO_MODER_MODE6 | GPIO_MODER_MODE7 | GPIO_MODER_MODE8);
-
-  GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD0 | GPIO_PUPDR_PUPD1 | GPIO_PUPDR_PUPD2 | GPIO_PUPDR_PUPD3 | GPIO_PUPDR_PUPD5 | GPIO_PUPDR_PUPD6 | GPIO_PUPDR_PUPD7 | GPIO_PUPDR_PUPD8);
-  GPIOA->PUPDR |= (GPIO_PUPDR_PUPD0_1 | GPIO_PUPDR_PUPD1_1 | GPIO_PUPDR_PUPD2_1 | GPIO_PUPDR_PUPD3_1);
-
-  // COLS  output HIGH
-  GPIOB->MODER &= ~(GPIO_MODER_MODE3 | GPIO_MODER_MODE4 | GPIO_MODER_MODE5);
-  GPIOB->MODER |= (GPIO_MODER_MODE3_0 | GPIO_MODER_MODE4_0 | GPIO_MODER_MODE5_0);
-  // set LOW in known state
-  GPIOB->ODR &= ~KEYPAD_COLS;
-
-
-  // Output LEDs
-  GPIOA->MODER |= (GPIO_MODER_MODE5_0 | GPIO_MODER_MODE6_0 | GPIO_MODER_MODE7_0 | GPIO_MODER_MODE8_0);
-  GPIOA->ODR &= ~(GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8);
+  init_gpio();
 
   while (1) {
-    int keypad_value = read_keypad();
+    // Initial keypad read
+    int16_t keypad_value = read_keypad();
+    // If it isn't an error, continue
     if (keypad_value != ERR_KEYPAD) {
       // 50ms debounce delay
       HAL_Delay(50);
-      int debounce_read = read_keypad();
+      int16_t debounce_read = read_keypad();
+      // If the input has changed, debounce has failed (input differs)
       if (keypad_value != debounce_read) continue;
 
       // Valid press
-      int out = keypad_value << 5;
-      GPIOA->ODR &= ~(GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8);
+      // Shift output into correct position in ODR
+      // (bits 5 to 8, not 0 to 3)
+      int16_t out = keypad_value << 5;
+      GPIOA->ODR &= ~LED_PIN_MASK;
       GPIOA->ODR |= out;
     }
 
@@ -93,43 +63,74 @@ int main(void) {
   }
 }
 
-int read_keypad() {
+void init_gpio() {
+  // ROWS  input w/ pull-downs
+  // PA0, PA1, PA9, PA10
+  // Tried using PA2 and PA3,
+  // but it seems to cause issues with the uart TX/RX
+  GPIOA->MODER &= ~(
+                    GPIO_MODER_MODE0 | GPIO_MODER_MODE1 | 
+                    GPIO_MODER_MODE9 | GPIO_MODER_MODE10 | 
+                    GPIO_MODER_MODE5 | GPIO_MODER_MODE6 | 
+                    GPIO_MODER_MODE7 | GPIO_MODER_MODE8
+                  );
+  // Reset PUPDR
+  GPIOA->PUPDR &= ~(
+                    GPIO_PUPDR_PUPD0 | GPIO_PUPDR_PUPD1 | 
+                    GPIO_PUPDR_PUPD9 | GPIO_PUPDR_PUPD10 | 
+                    GPIO_PUPDR_PUPD5 | GPIO_PUPDR_PUPD6 | 
+                    GPIO_PUPDR_PUPD7 | GPIO_PUPDR_PUPD8
+                  );
+  // Set Rows to Pull-down (pushing button sends row high --- cols as inputs)
+  GPIOA->PUPDR |= (
+                    GPIO_PUPDR_PUPD0_1 | GPIO_PUPDR_PUPD1_1 | 
+                    GPIO_PUPDR_PUPD9_1 | GPIO_PUPDR_PUPD10_1
+                  );
 
-  for (int c = 0; c < 3; c++) {
-    // Drive this column high, all others low
+  // COLS  output HIGH
+  // PB3, PB4, PB5
+  GPIOB->MODER &= ~(GPIO_MODER_MODE3 | GPIO_MODER_MODE4 | GPIO_MODER_MODE5);
+  GPIOB->MODER |= (GPIO_MODER_MODE3_0 | GPIO_MODER_MODE4_0 | GPIO_MODER_MODE5_0);
+  GPIOB->OTYPER &= (GPIO_OTYPER_OT3 | GPIO_OTYPER_OT4 | GPIO_OTYPER_OT5);
+  // set LOW in known state
+  GPIOB->ODR &= ~KEYPAD_COLS;
+
+
+  // Output LEDs
+  // PA5, PA6, PA7, PA8
+  GPIOA->MODER |= (GPIO_MODER_MODE5_0 | GPIO_MODER_MODE6_0 | GPIO_MODER_MODE7_0 | GPIO_MODER_MODE8_0);
+  GPIOA->ODR &= ~LED_PIN_MASK;
+}
+
+int16_t read_keypad() {
+
+  for (uint8_t col = 0; col < NUM_COLS; col++) {
+    // Drive current column high, all others low
     GPIOB->ODR &= ~KEYPAD_COLS;
-    GPIOB->ODR |= COL_PINS[c];
+    GPIOB->ODR |= COL_PINS[col];
     HAL_Delay(1);
     
-    // Check each row
-    for (int r = 0; r < 4; r++) {
-      if (GPIOA->IDR & ROW_PINS[r]) {
-        return keypad_map[r][c];
+    // Check each row to see if specific button is pressed
+    for (uint8_t row = 0; row < NUM_ROWS; row++) {
+      if (GPIOA->IDR & ROW_PINS[row]) {
+        return keypad_map[row][col];
       }
     }
   }
 
+  // Otherwise return unknown value (-1)
   return ERR_KEYPAD;
 }
 
 
-/**
- * @brief System Clock Configuration
- * @retval None
- */
 void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-   */
   if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK) {
     Error_Handler();
   }
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -144,8 +145,6 @@ void SystemClock_Config(void) {
     Error_Handler();
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
                                 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -158,67 +157,13 @@ void SystemClock_Config(void) {
   }
 }
 
-/**
- * @brief USART2 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_USART2_UART_Init(void) {
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart2) != HAL_OK) {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
 void Error_Handler(void) {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1) {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
-/**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+
 void assert_failed(uint8_t *file, uint32_t line) {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line
-     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
-     line) */
-  /* USER CODE END 6 */
 }
-#endif /* USE_FULL_ASSERT */
+#endif
