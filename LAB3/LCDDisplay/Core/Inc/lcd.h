017133445dae67a85e7cc970bdb06053393819b2
seasaltsaige <oriontothemax@gmail.com>
2026-04-15 01:41:16 -0700

    Mostly working, get write string working better later, add \n support, but also overflow...

--- LAB3/LCDDisplay/Core/Inc/lcd.h
+++ LAB3/LCDDisplay/Core/Inc/lcd.h
@@ -5,26 +5,39 @@
 #include <stdint.h>
 
 #define LCD_DATA_PIN_MASK (GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7)
+extern const 
 #define LCD_RS (GPIO_PIN_8)
 #define LCD_RW (GPIO_PIN_9)
 #define LCD_E (GPIO_PIN_10)
 
+#define LCD_PORT GPIOB
+
+#define LCD_CMD_FS 0x38
+#define LCD_CMD_DISP 0x0F
+#define LCD_CMD_CLEAR 0x01
+#define LCD_CMD_ENTRY 0x06
+#define LCD_CMD_HOME 0x02
+
+#define LCD_DDRAM_SET 0x80
+
+
+#define LCD_MAX_CHARS 32
+
+#define TYPING_SPEED 250
+
+
 // R/W pin defs
-#define LCD_READ 0
-#define LCD_WRITE 1
 
 // RS pin defs
-#define LCD_RS_INST 0
-#define LCD_RS_DATA 1
 
 // E pin defs
-#define LCD_E_EN 1
-#define LCD_E_DIS 0
+
 
 #define W_CMD 0
 #define W_DAT 1
 
 void LCD_startup();
+void LCD_write_string(char* data, uint8_t length);
 void LCD_write(uint8_t data, uint8_t mode);
 
 #endif
--- LAB3/LCDDisplay/Core/Src/lcd.c
+++ LAB3/LCDDisplay/Core/Src/lcd.c
@@ -1,27 +1,108 @@
 #include "lcd.h"
 #include "stm32l4xx_hal.h"
+#include <stddef.h>
 #include <stdint.h>
 
-void LCD_startup() {
-    // Wakeup data
-    uint8_t output_data = 0x30;
 
-    LCD_write(output_data, W_CMD);
-    // Wait 100ms
+uint8_t row_offsets[4] = {
+    0x00,
+    0x40,
+    0x10,
+    0x50
+};
+
+
+void LCD_startup() {
+    
+    // Wait for more than 30ms on init
     HAL_Delay(100);
 
-    LCD_write(output_data, W_CMD);
-    // Wait 1ms
-    HAL_Delay(10);
+    LCD_write(LCD_CMD_FS, W_CMD);
+    
+    // At least 40us
+    HAL_Delay(1);
+    // 0000 1111 = Disp on/off, disp=ON, curs=ON, blink=ON
+    LCD_write(LCD_CMD_DISP, W_CMD);
 
-    // Wait 1ms
+    // At least 40us
+    HAL_Delay(1);
+    // 0000 0001 = Clear display
+    LCD_write(LCD_CMD_CLEAR, W_CMD);
+
+    // At least 1.64ms
     HAL_Delay(10);
+    // 0000 0110 = Entry mode, increment mode, shift off
+    LCD_write(LCD_CMD_ENTRY, W_CMD);
+
+    return;
+}
+
+void LCD_write_string(char* data, uint8_t length) {
+    LCD_write(LCD_CMD_CLEAR, W_CMD);
+    LCD_write(LCD_CMD_HOME, W_CMD);
+
+    uint8_t i;
+    // Write top half
+    for (i = 0; i < (LCD_MAX_CHARS / 2) ; i++) {
+        if (i == length) return;
+        // If space, move right instead of write
+        // if (data[i] == ' ')
+        //     LCD_write(0x14, W_CMD);
+        // else if (data == '\0')
+        //     continue;
+        // else
+        
+        if (data[i] <= 2 || data[i] >= 0xFF) continue;
+            LCD_write(data[i], W_DAT);
+
+        HAL_Delay(TYPING_SPEED);
+    }
+
+    uint8_t cols = 16;
+    uint8_t rows = 2;
+    uint8_t move_cursor = LCD_DDRAM_SET | (0x40);
+    LCD_write(move_cursor, W_CMD);
+    
+    for (; i < LCD_MAX_CHARS; i++) {
+        if (i == length) return;
+        // If space, move right instead of write
+        // if (data[i] == ' ')
+        //     LCD_write(0x14, W_CMD);
+        // else if (data == '\0')
+        //     continue;
+        if (data[i] <= 2 || data[i] >= 0xFF) continue;
+        // else
+            LCD_write(data[i], W_DAT);
+        
+        HAL_Delay(TYPING_SPEED);
+    }
+
+    return;
 }
 
 void LCD_write(uint8_t data, uint8_t mode) {
     // Disable everything
-    GPIOA->BRR |= (LCD_DATA_PIN_MASK | LCD_RW | LCD_RS | LCD_E);
+    LCD_PORT->BRR |= (LCD_DATA_PIN_MASK | LCD_RW | LCD_RS | LCD_E);
+    HAL_Delay(1);
 
-    GPIOA->BSRR |= (data);
-    // GPIOA->BRR
+    LCD_PORT->BSRR |= (data);
+    HAL_Delay(1);
+    // Set RS to 0 if sending command/instruction
+    if (mode == W_CMD)
+        LCD_PORT->BRR |= (LCD_RS);
+    // Otherwise set to 1 for data write
+    else 
+        LCD_PORT->BSRR |= (LCD_RS);
+    HAL_Delay(1);
+    // Write opperation
+    LCD_PORT->BRR |= (LCD_RW);
+    
+    HAL_Delay(1);
+
+    // Set enable pin high
+    LCD_PORT->BSRR |= (LCD_E);
+    HAL_Delay(1);
+    // Reset
+    LCD_PORT->BRR |= (LCD_E);
+    return;
 }
