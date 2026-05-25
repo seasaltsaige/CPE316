6a063fee225580a7954a96d0e0665790f77ed008
seasaltsaige <oriontothemax@gmail.com>
2026-05-24 17:22:04 -0700

    work on sleep modes

--- LAB15/LowPowerModes/Core/Src/main.c
+++ LAB15/LowPowerModes/Core/Src/main.c
@@ -15,99 +15,117 @@
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "stm32l476xx.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-UART_HandleTypeDef huart2;
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
-/* USER CODE BEGIN PFP */
+void NormalMode();
+void SleepMode();
+void Stop2_RTC_Config();
+void Stop2Mode();
 
-/* USER CODE END PFP */
+void BusyWork();
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
+// SLEEP_TYPE = 0 | Normal
+// SLEEP_TYPE = 1 | Sleep Mode
+// SLEEP_TYPE = 2 | Stop 2
+#define SLEEP_TYPE 0
 
-/* USER CODE END 0 */
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  // Default setup
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+  // Ensure peripheral clocks are disabled for testing
+  RCC->AHB2ENR &= ~(
+    RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN |
+    RCC_AHB2ENR_GPIOCEN | RCC_AHB2ENR_GPIODEN | 
+    RCC_AHB2ENR_GPIOEEN | RCC_AHB2ENR_GPIOFEN | 
+    RCC_AHB2ENR_GPIOGEN | RCC_AHB2ENR_GPIOHEN
+  );
 
-  /* USER CODE END SysInit */
+  if (SLEEP_TYPE == 0) {
+    
+  } else if (SLEEP_TYPE == 1) {
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART2_UART_Init();
-  /* USER CODE BEGIN 2 */
+  } else if (SLEEP_TYPE == 2) {
 
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+  } else
+    // ...
+    while (1) {};
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+void NormalMode() {
+  while (1) { __NOP(); };
+}
+
+void SleepMode() {
+  SCB->SCR &= ~(SCB_SCR_SLEEPDEEP_Msk);
+  while (1) { __WFI(); };
+}
+
+void Stop2_RTC_Config() {
+  // Unlock backup domain
+  RCC->APB1ENR1 |= RCC_APB1ENR1_PWREN;
+  PWR->CR1 |= PWR_CR1_DBP;
+  while (!(PWR->CR1 & PWR_CR1_DBP)) {};
+
+  // enable LSI 32khz
+  RCC->CSR |= RCC_CSR_LSION;
+  // Wait for stable
+  while (!(RCC->CSR & RCC_CSR_LSIRDY)) {};
+
+  // Select LSI as rtc clk
+  RCC->BDCR &= ~(RCC_BDCR_RTCSEL);
+  RCC->BDCR |= (RCC_BDCR_RTCSEL_1 | RCC_BDCR_RTCEN);
+
+  // write protection key sequence
+  // stm32l47xxx ref manual page 1232 (section 38.3.7)
+  // two byte sequence
+  RTC->WPR = 0xCA;
+  RTC->WPR = 0x53;
+
+  RTC->CR &= ~(RTC_CR_WUTE);
+  while (!(RTC->ISR & RTC_ISR_WUTWF)) {};
+
+  // Set wakeup clk sel to ck_spre
+  RTC->CR &= ~(RTC_CR_WUCKSEL);
+  RTC->CR |= (RTC_CR_WUCKSEL_2);
+
+  // Set wake timer to 5 seconds (based on 1hz clk sel above)
+  RTC->WUTR = 4; // (WUTR + 1)
+
+  // re-enable wakeup timer + irq
+  RTC->CR |= (RTC_CR_WUTE | RTC_CR_WUTIE);
+
+  // write to WPR; re-enable write protection
+  RTC->WPR = 0xFF;
+
+  // bit 20 for rtc wakeup timer
+  // rising edge wakeup
+  EXTI->IMR1 |= EXTI_IMR1_IM20;
+  EXTI->RTSR1 |= EXTI_RTSR1_RT20;
+
+  // set priority and enable in nvic
+  NVIC->IP[RTC_WKUP_IRQn] = 0x00;
+  NVIC->ISER[0] |= (1 << RTC_WKUP_IRQn);
+
+  __enable_irq();
+}
+
+
+void BusyWork(void)
+{
+  volatile uint32_t i;
+  volatile uint64_t result = 0;
+  for (i = 0; i < 100000; i++)
+  {
+    result += i * 3;
+  }
+}
+
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -164,111 +182,17 @@ void SystemClock_Config(void)
   HAL_RCCEx_EnableMSIPLLMode();
 }
 
-/**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART2_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+
 }
-#endif /* USE_FULL_ASSERT */
+#endif
